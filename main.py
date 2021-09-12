4050fb87387e1abe5823a30c54c85e7a372c8222
Saif Hossain <atonlymesaif@gmail.com>
2021-09-12 14:09:21 +0600

    batch compression of video files in folder added

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+import subprocess, shlex
+
+files_directory = input("Enter directory path: ")
+
+new_directory_path = os.path.join(files_directory, "Compressed")
+try:
+    os.mkdir(new_directory_path)
+except FileExistsError:
+    print("Directory already exists, copying file in the existing directory")
+
+files_list = os.listdir(files_directory)
+
+for file_name in files_list:
+    try:
+        is_directory = os.path.isdir(files_directory + "/" + file_name)
+        base_file_name = Path(file_name).stem
+
+        input_file = os.path.join(files_directory, file_name)
+        output_file = os.path.join(new_directory_path, base_file_name + ".mp4")
+        command = 'ffmpeg -i ' + '"' + input_file + '"' + ' -vcodec libx265 -crf 28 ' + '"' + output_file + '"'
+
+        print(command)
+
+        if not is_directory:
+            os.system(command)
+    except FileNotFoundError:
+        print('Error occurred')
